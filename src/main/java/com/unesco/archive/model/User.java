ff58c8fb4bb2cf8cab42f142e3ba495e83476c7a
letlabs <letlabs.ndj@gmail.com>
2024-06-09 23:44:46 +0100

    upload,update and archive deletion api done

---
+++ src/main/java/com/unesco/archive/model/User.java
@@ -0,0 +1,29 @@
+package com.unesco.archive.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor @NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    
+}
